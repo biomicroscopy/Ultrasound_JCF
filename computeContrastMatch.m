f9d4b83e7ec9a8a79c39702cdaff2326a8aef4b3
nikunjkhetan <nikunj101@gmail.com>
2024-04-18 18:27:52 -0400

    Added Contrast Matching function

---
+++ computeContrastMatch.m
@@ -0,0 +1,16 @@
+function [g] = computeContrastMatch(refIm,targIm,cFactor)
+
+    img=abs(refIm).^cFactor;
+    img(isinf(img)) = 0;
+    img(isnan(img)) = 0;
+    mB=mean(img,'all');
+    kB=var(img,[],'all')/mB^2;
+
+    g0=0.05;
+    imdat = abs(targIm);
+    imdat(isinf(imdat)) = 0;
+    imdat(isnan(imdat)) = 0;
+    F = @(x) [kB-var(imdat.^x,[],'all')/mean(imdat.^x,'all')^2];
+    g=fsolve(F,g0);
+
+end
